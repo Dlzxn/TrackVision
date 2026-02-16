cebade2c4d6e9c3f2d6b619f29e841697d8c2612
Dlzxn <aleshamarichev09@gmail.com>
2026-02-16 13:38:37 +0300

    start dir

---
+++ ModelServiceAI/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from modelserviceai!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from trackvision!")
+
+
+if __name__ == "__main__":
+    main()
